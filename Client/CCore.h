a3f1408a7bac6ac7f720b6d906a13fdffa21bc77
RedSudal <parkcb1950@gmail.com>
2022-08-27 19:42:56 +0900

    1

--- Client/CCore.h
+++ Client/CCore.h
@@ -5,35 +5,53 @@
 // 객체의 생성을 1개로 제한
 // 어디서든 쉽게 접근 가능
 
+//class CCore
+//{
+//private:
+//	//static CCore* g_pInst;
+//
+//public:
+//	// 정적 맴버함수
+//	static CCore* GetInstance()
+//	{
+//		// 최초 호출 된 경우	
+//		if (nullptr == g_pInst)
+//		{
+//			g_pInst = new CCore;
+//		}
+//		
+//		return g_pInst;
+//	}
+//
+//	static void Release()
+//	{
+//		if (nullptr != g_pInst)
+//		{
+//			delete g_pInst;
+//			g_pInst = nullptr;
+//		}
+//		
+//	}
+//
+//private:
+//	CCore();
+//	~CCore();
+//};
+
+
 class CCore
 {
+	SINGLE(CCore);
 private:
-	static CCore* g_pInst;
-
+	HWND	m_hWnd;
+	POINT	m_ptResolution;
 public:
-	// 정적 맴버함수
-	static CCore* GetInstance()
-	{
-		// 최초 호출 된 경우	
-		if (nullptr == g_pInst)
-		{
-			g_pInst = new CCore;
-		}
-		
-		return g_pInst;
-	}
+	int init(HWND _hWnd, POINT _ptResolution);
+	void progress();
 
-	static void Release()
-	{
-		if (nullptr != g_pInst)
-		{
-			delete g_pInst;
-			g_pInst = nullptr;
-		}
-		
-	}
 
 private:
 	CCore();
 	~CCore();
+
 };
--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,8 +1,10 @@
 ﻿// Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
+#include "pch.h"
 #include "framework.h"
 #include "Client.h"
+#include "CCore.h"
 
 #define MAX_LOADSTRING 100
 
@@ -10,6 +12,7 @@
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HWND g_hWnd;
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -30,11 +33,21 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_CLIENT, szWindowClass, MAX_LOADSTRING);
+    
+    // 윈도우 정보 등록
     MyRegisterClass(hInstance);
 
     // 애플리케이션 초기화를 수행합니다:
     if (!InitInstance (hInstance, nCmdShow))
     {
+        return FALSE;
+    }
+
+    
+    // Core 초기화
+    if (FAILED(CCore::GetInst()->init(g_hWnd, POINT{1280, 768})))
+    {
+        MessageBox(nullptr, L"Core 객체 초기화 실패", L"Error", MB_OK);
         return FALSE;
     }
 
@@ -63,13 +76,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                 DispatchMessage(&msg);
             }
         }
+
+        // 메세지가 발생하지 않는 대부분의 시간
         else
         {
-
             //Game 코드 수행
             //디자인 패턴(설계 유형)
             //싱글톤 패턴
-
+            CCore::GetInst()->progress();
         }
     }
 
@@ -118,16 +132,16 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+    g_hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
+   if (!g_hWnd)
    {
       return FALSE;
    }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+   ShowWindow(g_hWnd, nCmdShow);
+   UpdateWindow(g_hWnd);
 
    return TRUE;
 }
